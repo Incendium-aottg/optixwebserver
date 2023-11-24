07d82bd5ed76e6742807c311141284f692722728
Incendium-aottg <infernodescent@gmail.com>
2023-11-24 12:48:04 -0500

    fixed styling

--- src/optix/optix.component.ts
+++ src/optix/optix.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'optix-root',
-  templateUrl: './optix.component.html',
-  styleUrls: ['./optix.component.sass']
+	selector: 'optix-root',
+	templateUrl: './optix.component.html',
+	styleUrls: ['./optix.component.sass']
 })
+
 export class OptixComponent {
-  title = 'optixwebserver';
+	title = 'optixwebserver';
+
+	constructor(private router: Router) {}
+
+	isHomeRoute() {
+		return this.router.url === '/';
+	}
 }
